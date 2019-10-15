4797e782a9d1862ac52825a848b9e4b62d16695f
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-15 04:20:20 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,6 @@ class Appointment
   attr_accessor :date, :patient, :doctor
   @@all = []
   
- # def initialize(patient, date, doctor)
  def initialize(date, patient, doctor)
     @date = date
     @patient = patient
