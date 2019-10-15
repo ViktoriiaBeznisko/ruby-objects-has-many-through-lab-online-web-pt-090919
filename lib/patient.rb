38e9ddb181654615e1cb3f9f1f36ca03b821b155
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-15 04:19:52 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,7 @@ class Patient
     
    def new_appointment(doctor, date)
     #Appointment.new(self, date, doctor)
-    Appointment.new(self, date, doctor)
+    Appointment.new(date, self, doctor)
    end
     
     def appointments
